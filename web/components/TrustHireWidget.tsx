30b24be5ee1998cf4a97baf79f6ace9334ba203e
Michael Laberge <michaellaberge@Mac.ht.home>
2026-01-30 19:05:47 -0500

    Adjustment: Increase launcher desktop spacing (sm:bottom-6 -> sm:bottom-9) to visually add ~10px margin

--- web/components/TrustHireWidget.tsx
+++ web/components/TrustHireWidget.tsx
@@ -205,7 +205,7 @@ export function TrustHireWidget({ slug }: TrustHireWidgetProps) {
     if (!isOpen) {
         return (
             <div
-                className={`fixed bottom-0 right-0 sm:bottom-6 sm:right-6 z-50 ${!hasPlayedAnimation ? 'animate-slide-in-from-right' : ''}`}
+                className={`fixed bottom-0 right-0 sm:bottom-9 sm:right-9 z-50 ${!hasPlayedAnimation ? 'animate-slide-in-from-right' : ''}`}
                 style={!hasPlayedAnimation ? { animationDelay: '1s', animationFillMode: 'both' } : {}}
             >
                 <MinimizedBar onOpen={handleOpen} onClose={handleClose} />
